5f86f9503eb088a4937adea129141ec647041876
Sen Chen <senchen@CNsenchen.local>
2018-07-17 08:43:09 +0800

    latst successful

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,35 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  let res = new Array();
+  collection.forEach(element => {
+    res.push(num2str(element));
+  });
+  return res;
 };
+
+function num2str(num) {
+  let stringName = "";
+  if(num > 0) {
+    if(num >= 1 && num <= 26) {
+      stringName = String.fromCharCode(96 + parseInt(num));
+    } else {
+      while(num > 26) {
+        var count = parseInt(num/26);
+        var remainder = num%26;
+        if(remainder == 0) {
+          remainder = 26;
+          count--;
+          stringName = String.fromCharCode(96 + parseInt(remainder)) + stringName;
+        } else {
+          stringName = String.fromCharCode(96 + parseInt(remainder)) + stringName;
+        }
+        num = count;
+      }
+      stringName = String.fromCharCode(96 + parseInt(num)) + stringName;
+    }
+  }
+
+  return stringName;
+}
 
 module.exports = number_map_to_word;
--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,16 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+  for(let i=0; i<collection.length; i++) {
+    for(let j=i; j<collection.length; j++) {
+        if(collection[i]  > collection[j]) {
+            let tmp = collection[i];
+            collection[i] = collection[j];
+            collection[j] = tmp;
+        }
+    }
+  }
+  return collection;
+  //return [2,3,4,5,6];
 };
 
 module.exports = rank_desc;
